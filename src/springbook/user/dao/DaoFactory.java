0e9cbdce7e00d2180ef8e11b67570f7edd3859ba
darkbast <darkbast@naver.com>
2019-11-10 00:55:55 +0900

    각 연결이 필요한 DaoFactory를 생성 후 커넥션을 DaoFactory별로 설정하는 것으로 쉽게 다른 회사나 조직, 개인으로 연결이 용이하게 된다.

--- src/springbook/user/dao/DaoFactory.java
+++ src/springbook/user/dao/DaoFactory.java
@@ -4,8 +4,19 @@ import java.sql.SQLException;
 
 public class DaoFactory {
 	public UserDao userDao() throws ClassNotFoundException, SQLException{
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		UserDao userDao = new UserDao(connectionMaker);
+		UserDao userDao = new UserDao(getConnectionMaker());
 		return userDao;
 	}
+	
+	// 각종 Dao들이 존재할때마다 ConnectionMaker를 생성하게 된다.
+	// 커넥션을 지정하는 것으로 DaoFactory를 복수 지정하는 것만으로
+	// 다른 커넥션으로 연결되는 DaoFactory를 만들 수 있다.
+//	public ConnectionMaker getConnectionMaker(){
+//		return new 각 커넥션들;
+//	}
+	
+	public ConnectionMaker getConnectionMaker(){
+		return new DConnectionMaker();
+	}
+	
 }
