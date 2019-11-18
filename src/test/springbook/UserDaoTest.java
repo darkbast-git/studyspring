20736ce3d72c508b772347d7319e525f4b10ba74
darkbast <darkbast@naver.com>
2019-11-19 02:36:03 +0900

    @setUp, @Before 로 공통 부분 줄이기

--- src/test/springbook/UserDaoTest.java
+++ src/test/springbook/UserDaoTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertThat;
 
 import java.sql.SQLException;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -14,27 +15,29 @@ import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
 public class UserDaoTest {
+	private UserDao dao;
+	private User user1;
+	private User user2;
+	private User user3;
+	
+	@Before
+	public void setUp(){
+		ApplicationContext context = 
+				new GenericXmlApplicationContext("applicationContext.xml");
+		this.dao = context.getBean("userDao", UserDao.class);
+		this.user1 = new User("1","N1","P1");
+		this.user2 = new User("2","N2","P2");
+		this.user3 = new User("3","N3","P3");
+	}
 	
 	@Test
 	public void delelteAll() throws Exception{
-		GenericXmlApplicationContext context = 
-				new GenericXmlApplicationContext("applicationContext.xml");
-		
-		UserDao dao = context.getBean("userDao", UserDao.class);
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
 	}
 
 	@Test
 	public void addAndGet() throws ClassNotFoundException, SQLException{
-		GenericXmlApplicationContext context = 
-				new GenericXmlApplicationContext("applicationContext.xml");
-		
-		UserDao dao = context.getBean("userDao", UserDao.class);
-//		
-		User user1 = new User("1-1","N1-1","P1-1");
-		User user2 = new User("1-2","N1-2","P1-2");
-		
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
 		
@@ -54,13 +57,10 @@ public class UserDaoTest {
 	
 	@Test
 	public void count() throws Exception{
-		GenericXmlApplicationContext context = 
-				new GenericXmlApplicationContext("applicationContext.xml");
-		UserDao dao = context.getBean("userDao", UserDao.class);
 		
-		User user1 = new User("1","N1","P1");
-		User user2 = new User("2","N2","P2");
-		User user3 = new User("3","N3","P3");
+//		User user1 = new User("1","N1","P1");
+//		User user2 = new User("2","N2","P2");
+//		User user3 = new User("3","N3","P3");
 		
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
@@ -79,9 +79,6 @@ public class UserDaoTest {
 	// get()예외조건에 대한 테스트
 	@Test(expected=EmptyResultDataAccessException.class)
 	public void getUserFailure() throws Exception{
-		ApplicationContext context = 
-				new GenericXmlApplicationContext("applicationContext.xml");
-		UserDao dao = context.getBean("userDao", UserDao.class);
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
 		
